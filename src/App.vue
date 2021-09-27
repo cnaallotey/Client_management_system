fd68fcce188717b55414ee35fe24088c64f90dc1
Roberta Akoto <baffouraduboampong@gmail.com>
2021-09-27 17:39:16 +0000

    First Commit. Hurray

---
+++ src/App.vue
@@ -0,0 +1,333 @@
+<template>
+  <div class="flex">
+    <div class="w-96 h-screen bg-blue-900"><sidebar></sidebar></div>
+    <div class="bg-gray-200 w-full h-screen p-10 space-y-10 overflow-y-scroll">
+      <div class="flex flex-wrap overflow-hidden">
+        <div class="w-1/2 overflow-hidden">
+          <h1 class="text-3xl font-regular">Admin Dashboard</h1>
+        </div>
+
+        <div class="w-1/2 overflow-hidden flex justify-end">
+          <div></div>
+          <div class="flex items-center space-x-3 border-l-2 border-gray-500 pl-5">
+            <h1 class="font-medium text-gray-500 text-sm">charlesallotey@gmail.com</h1>
+            <div class="flex-shrink-0 h-10 w-10">
+              <img
+                class="h-10 w-10 rounded-full"
+                src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="w-full bg-white p-10 flex space-x-10">
+        <div class="relative">
+          <select
+            class="appearance-none h-full rounded-r border sm:rounded-r-none block w-full bg-gray-300 text-gray-700 py-2 px-4 pr-8 leading-tight focus:outline-none focus:border-r focus:border-gray-500"
+          >
+            <option>All</option>
+            <option>Active</option>
+            <option>Inactive</option>
+          </select>
+          <div
+            class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
+          >
+            <svg
+              class="fill-current h-4 w-4"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+            >
+              <path
+                d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"
+              />
+            </svg>
+          </div>
+        </div>
+        <div class="flex flex-1 border rounded bg-gray-300 items-center p-2">
+          <svg
+            class="fill-current text-gray-800 mr-2 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+          >
+            <path
+              class="heroicon-ui"
+              d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"
+            />
+          </svg>
+          <input
+            type="text"
+            placeholder="Search clients"
+            class="bg-gray-300 max-w-full focus:outline-none text-gray-700"
+            v-model="search"
+          />
+        </div>
+        <button
+          class="bg-blue-600 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded"
+          @click="newUser()"
+        >
+          Add new client
+        </button>
+      </div>
+      <!-- This example requires Tailwind CSS v2.0+ -->
+      <div class="flex flex-col">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="shadow overflow-hidden border-b border-gray-200">
+              <table class="min-w-full divide-y divide-gray-200">
+                <thead class="bg-gray-50">
+                  <tr>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Name
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Title
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Status
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Role
+                    </th>
+                    <th
+                      scope="col"
+                      class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                    >
+                      Actions
+                    </th>
+                  </tr>
+                </thead>
+                <tbody class="bg-white divide-y divide-gray-200">
+                  <tr
+                    v-for="employee in searchEmployees"
+                    :key="employee.id"
+                    class="hover:bg-blue-50"
+                  >
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <div class="flex items-center">
+                        <div class="flex-shrink-0 h-10 w-10">
+                          <img
+                            class="h-10 w-10 rounded-full"
+                            src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=4&w=256&h=256&q=60"
+                            alt=""
+                          />
+                        </div>
+                        <div class="ml-4">
+                          <div class="text-sm font-medium text-gray-900">
+                            <span>{{ employee.Name.firstName }}</span>
+                            {{ employee.Name.lastName }}
+                          </div>
+                          <div class="text-sm text-gray-500">{{ employee.email }}</div>
+                        </div>
+                      </div>
+                    </td>
+                    <t d class="px-6 py-4 whitespace-nowrap">
+                      <div class="text-sm font-medium text-gray-900">
+                        {{ employee.role }}
+                      </div>
+                      <div class="text-sm text-gray-500">{{ employee.department }}</div>
+                    </t>
+                    <td class="px-6 py-4 whitespace-nowrap">
+                      <span
+                        class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800"
+                      >
+                        Active
+                      </span>
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      Admin
+                    </td>
+                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium">
+                      <a
+                        href="#"
+                        class="text-green-600 hover:text-green-900 mr-5"
+                        @click="editClient(employee)"
+                        >Edit</a
+                      >
+                      <a
+                        href="#"
+                        class="text-red-600 hover:text-indigo-900"
+                        @click="deleteClient(employee)"
+                        >Delete Client</a
+                      >
+                    </td>
+                  </tr>
+
+                  <!-- More people... -->
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <modal
+            v-if="showmodal"
+            :pushUser="pushUser"
+            :toggleModal="toggleModal"
+            :error="error"
+            :firstName="firstName"
+            :lastName="lastName"
+            :role="role"
+            :department="department"
+            :email="email"
+            v-on:childToFirst="childFirst"
+            v-on:childToLast="childLast"
+            v-on:childToEmail="childEmail"
+            v-on:childToDep="childDep"
+            v-on:childToRole="childRole"
+          ></modal>
+          <editmodal
+            v-if="editmodal"
+            :pushUser="pushUser"
+            :error="error"
+            :firstName="firstName"
+            :lastName="lastName"
+            :role="role"
+            :department="department"
+            :email="email"
+            v-on:childToFirst="childFirst"
+            v-on:childToLast="childLast"
+            v-on:childToEmail="childEmail"
+            v-on:childToDep="childDep"
+            v-on:childToRole="childRole"
+          ></editmodal>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import json from "./clients.json";
+import modal from "./components/modal.vue";
+import editmodal from "./components/editmodal.vue";
+import sidebar from "./components/sidebar.vue";
+export default {
+  components: { modal, sidebar, editmodal },
+  data() {
+    return {
+      // make: "",
+      // model: "",
+      // year: "",
+      // car: "",
+      // cars: ["toyota", "hyundai", "mazda"],
+      showmodal: false,
+      editmodal: false,
+      employees: json,
+      firstName: "",
+      lastName: "",
+      email: "",
+      role: "",
+      department: "",
+      fromchild: "",
+      error: false,
+      editerror: false,
+      search: "",
+    };
+  },
+  methods: {
+    newUser: function () {
+      this.firstName = this.lastName = this.email = this.role = this.department = "";
+      setTimeout(() => {
+        this.showmodal = true;
+      }, 1000);
+    },
+    pushUser: function () {
+      if (
+        !this.firstName ||
+        !this.lastName ||
+        !this.role ||
+        !this.department ||
+        !this.email
+      ) {
+        this.error = true;
+      } else {
+        this.employees.push({
+          Name: { firstName: this.firstName, lastName: this.lastName },
+          department: this.department,
+          email: this.email,
+          role: this.role,
+          status: "active",
+          id: Math.floor(Math.random() * 100),
+        });
+        this.firstName = this.lastName = this.email = this.role = this.department = "";
+        this.error = false;
+        this.editmodal = false;
+        setTimeout(() => {
+          this.showmodal = false;
+        }, 1000);
+      }
+    },
+    updateuser: function () {
+      if (
+        !this.firstName ||
+        !this.lastName ||
+        !this.role ||
+        !this.department ||
+        !this.email
+      ) {
+        this.editerror = true;
+      } else {
+      }
+    },
+    editClient: function (employee, thing) {
+      this.editmodal = true;
+      this.firstName = employee.Name.firstName;
+      this.lastName = employee.Name.lastName;
+      this.role = employee.role;
+      this.department = employee.department;
+      this.email = employee.email;
+      this.editmodal = true;
+
+      this.employees = this.employees.filter((item) => item.id !== employee.id);
+    },
+    toggleModal: function () {
+      this.showmodal = false;
+    },
+    toggleEditModal: function () {},
+    deleteClient: function (thing) {
+      this.employees = this.employees.filter((item) => item.id !== thing.id);
+    },
+    childFirst: function (value) {
+      this.firstName = value;
+    },
+    childLast: function (value) {
+      this.lastName = value;
+    },
+    childRole: function (value) {
+      this.role = value;
+    },
+    childEmail: function (value) {
+      this.email = value;
+    },
+    childDep: function (value) {
+      this.department = value;
+    },
+  },
+  // updated() {
+  //   this.employees = this.employees.sort((a, b) => (a.firstName > b.lastName && 1) || -1);
+  // },
+
+  computed: {
+    searchEmployees() {
+      return this.employees.filter((employee) => {
+        return employee.Name.firstName.match(this.search);
+      });
+    },
+  },
+  directives: {},
+};
+</script>
---
+++ src/components/modal.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div
+      class="fixed z-10 inset-0 overflow-y-auto"
+      aria-labelledby="modal-title"
+      role="dialog"
+      aria-modal="true"
+    >
+      <div
+        class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
+      >
+        <!--
+      Background overlay, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0"
+        To: "opacity-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100"
+        To: "opacity-0"
+    -->
+        <div
+          class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+          @click="toggleModal"
+          aria-hidden="true"
+        ></div>
+
+        <!-- This element is to trick the browser into centering the modal contents. -->
+        <span
+          class="hidden sm:inline-block sm:align-middle sm:h-screen"
+          aria-hidden="true"
+          >&#8203;</span
+        >
+
+        <!--
+      Modal panel, show/hide based on modal state.
+
+      Entering: "ease-out duration-300"
+        From: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+        To: "opacity-100 translate-y-0 sm:scale-100"
+      Leaving: "ease-in duration-200"
+        From: "opacity-100 translate-y-0 sm:scale-100"
+        To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+    -->
+        <div
+          class="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
+        >
+          <form action="" class="form bg-white p-6 my-10 relative">
+            <div
+              class="icon bg-blue-600 text-white w-6 h-6 absolute flex items-center justify-center p-5"
+              style="left: -40px"
+            >
+              <i class="fal fa-phone-volume fa-fw text-2xl transform -rotate-45"></i>
+            </div>
+            <h3 class="text-2xl text-gray-900 font-semibold">Add a new User</h3>
+            <div class="flex space-x-5 mt-3">
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="First name"
+                class="border p-2 w-1/2"
+                v-model="firstName"
+                v-on:keyup="emitToFirst"
+              />
+              <input
+                type="text"
+                name=""
+                id=""
+                placeholder="Last Name"
+                class="border p-2 w-1/2"
+                v-model="lastName"
+                v-on:keyup="emitToLast"
+              />
+            </div>
+            <input
+              type="email"
+              name=""
+              id=""
+              placeholder="Your Email"
+              class="border p-2 w-full mt-3"
+              v-model="email"
+              v-on:keyup="emitToEmail"
+            />
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Department"
+              class="border p-2 w-full mt-3"
+              v-model="department"
+              v-on:keyup="emitToDepartment"
+            />
+            <input
+              type="text"
+              name=""
+              id=""
+              placeholder="Role"
+              class="border p-2 w-full mt-3"
+              v-model="role"
+              v-on:keyup="emitToRole"
+            />
+            <div class="mt-4">
+              <p class="text-xs font-normal text-red-500" v-if="error">
+                Please fill all spaces
+              </p>
+            </div>
+            <!--<textarea name="" id="" cols="10" rows="3" placeholder="Tell us about desired property" class="border p-2 mt-3 w-full"></textarea>-->
+
+            <input
+              type="submit"
+              value="Submit"
+              class="w-full mt-6 bg-blue-600 hover:bg-blue-500 text-white font-semibold p-3"
+              @click.prevent="pushUser()"
+            />
+          </form>
+        </div>
+      </div>
+      <div class="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    pushUser: { type: Function },
+    error: { type: Boolean },
+    showModal: { type: Boolean },
+    toggleModal: { type: Function },
+    // firstName: { type: String },
+    // lastName: { type: String },
+    // department: { type: String },
+    // role: { type: String },
+    // email: { type: String },
+  },
+  data() {
+    return {
+      firstName: "",
+      lastName: "",
+      department: "",
+      role: "",
+      email: "",
+    };
+  },
+  methods: {
+    emitToFirst: function (event) {
+      this.$emit("childToFirst", this.firstName);
+    },
+    emitToLast: function (event) {
+      this.$emit("childToLast", this.lastName);
+    },
+    emitToDepartment: function (event) {
+      this.$emit("childToDep", this.department);
+    },
+    emitToEmail: function (event) {
+      this.$emit("childToEmail", this.email);
+    },
+    emitToRole: function (event) {
+      this.$emit("childToRole", this.role);
+    },
+  },
+};
+</script>
